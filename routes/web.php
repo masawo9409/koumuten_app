bf7583ddc63921c3356022bff4aa384a25a72b67
masawo9409 <masawo9409@gmail.com>
2022-10-24 16:03:25 +0900

    /をログイン画面に変更

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use App\Http\Controllers\KoumtenController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
