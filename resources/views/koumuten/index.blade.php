9ab2a59ef83339190464e2c675d70bd96eeddb32
masawo9409 <masawo9409@gmail.com>
2022-10-29 14:40:19 +0900

    cssやや進捗

--- resources/views/koumuten/index.blade.php
+++ resources/views/koumuten/index.blade.php
@@ -4,40 +4,44 @@
             工務店お探し
         </h2>
     </x-slot>
-    <div class="container max-w-7xl px-4 md:px-12 pb-3 mt-5 ml-3 mr-3 bg-white shadow-md">
-        <div class="container pt-3 pb-3">
-            <div class=" container font-bold pb-5 text-lg pt-3 pb-3 font-bold text-gray-700">
-                <p>{{ $constructs }} に対応できる会社一覧</p>
-            </div>
-            @foreach ($koumutens as $koumuten)
-                <div class="bg-gray-100 py-4 px-4 hover:text-gray-500">
-                    <a href="{{ route('koumtens.show', $koumuten) }}">
-                        <div class="font-bold">
-                            {{ $koumuten->name }}
-                        </div>
-                        <div class="block text-gray-700 text-sm mt-2">{{ $koumuten->address }}</div>
-                        <div class="block text-gray-700 text-sm mt-2">対応可能エリア</div>
-                        <h3>{{ $koumuten->avairable_area }}</h3>
-                        <div class="block text-gray-700 text-sm mt-2">{{ $constructs }}工事の予算(万円)</div>
-                        <h3>{{ $koumuten->sinchiku }}~</h3>
-                        <div class="block text-gray-700 text-sm mt-2">電話番号</div>
-                        <h3>{{ $koumuten->contact_address_phone }}</h3>
-                        <div class="block text-gray-700 text-sm mt-2">メールアドレス</div>
-                        <h3>{{ $koumuten->contact_address_mail }}</h3>
-                    </a>
-                </div>
-                <br>
-            @endforeach
-            <div class="block text-gray-700 text-sm mb-2 hover:text-gray-500">
-                <button input="submit" onclick="location.href='{{ route('top') }}'">TOPページに戻る</button>
-            </div>
-
-            <form action="{{ route('button1') }}">
-                <button type="submit" name="top" onclick="location.href='{{ route('button1') }}'">
-            </form>
-
+    <div class="px-4 md:px-12 pb-3 mt-5 ml-3 mr-3 bg-white shadow-md">
+        <div class="font-bold pb-5 text-lg pt-3 pb-3 font-bold text-gray-700">
+            <p>{{ $constructs }} に対応できる会社一覧</p>
         </div>
-        <div id="map" style="width:1000px; height:600px" class="flex"></div>
+        <div class="flex">
+            <div class="pt-3 pb-3">
+                @foreach ($koumutens as $koumuten)
+                    <div class="bg-gray-100 py-4 px-4 mr-5 hover:text-gray-500">
+                        <a href="{{ route('koumtens.show', $koumuten) }}">
+                            <div class="font-bold">
+                                {{ $koumuten->name }}
+                            </div>
+                            <div class="block text-gray-700 text-sm mt-2">{{ $koumuten->address }}</div>
+                            <div class="block text-gray-700 text-sm mt-2">対応可能エリア</div>
+                            <h3>{{ $koumuten->avairable_area }}</h3>
+                            <div class="block text-gray-700 text-sm mt-2">{{ $constructs }}工事の予算(万円)</div>
+                            <h3>{{ $koumuten->sinchiku }}~</h3>
+                            <div class="block text-gray-700 text-sm mt-2">電話番号</div>
+                            <h3>{{ $koumuten->contact_address_phone }}</h3>
+                            <div class="block text-gray-700 text-sm mt-2">メールアドレス</div>
+                            <h3>{{ $koumuten->contact_address_mail }}</h3>
+                        </a>
+                    </div>
+                    <br>
+                @endforeach
+                <div class="block text-gray-700 text-sm mb-2 hover:text-gray-500">
+                    <button input="submit" onclick="location.href='{{ route('top') }}'">TOPページに戻る</button>
+                </div>
+
+                <form action="{{ route('button1') }}">
+                    <button type="submit" name="top" onclick="location.href='{{ route('button1') }}'">
+                </form>
+
+            </div>
+            <div class="mt-3 h-2">
+                <div id="map" style="width:1000px; height:600px" class="flex"></div>
+            </div>
+        </div>
     </div>
 
     @include('partial.map')
--- resources/views/koumuten/show.blade.php
+++ resources/views/koumuten/show.blade.php
@@ -1,69 +1,74 @@
 <!-- 会社詳細が表示されるページ -->
 <x-app-layout>
-    <div class="container  px-4 md:px-12 pb-3 mt-5 ml-3 mr-3 bg-white shadow-md">
-
+    <div class="px-4 md:px-12 pb-3 mt-5 ml-3 mr-3 bg-white shadow-md">
         <div class="container text-lg pt-3 pb-3 font-bold text-gray-700">
             <p>{{ $koumten->name }}</p>
         </div>
-        <div class="block text-gray-700  mt-5 border-l-2 pl-2 mb-2">住所</div>
-        {{ $koumten->address }}
-        <div class="block text-gray-700  mt-5 mb-1 border-l-2 pl-2 mb-2">対応可能工事一覧 (単位:万円)</div>
-        <table>
-            <tr>
-                <td>新築&nbsp;&nbsp;</td>
-                <td>{{ $koumten->sinchiku }}&nbsp;~</td>
-            </tr>
-            <tr>
-                <td>リフォーム&nbsp;&nbsp;</td>
-                <td>{{ $koumten->reform }}&nbsp;~</td>
-            </tr>
-            <tr>
-                <td>修理(雨漏り)&nbsp;&nbsp;</td>
-                <td>{{ $koumten->repair_amamori }}&nbsp;~</td>
-            </tr>
-            <tr>
-                <td>修理(外壁)&nbsp;&nbsp;</td>
-                <td>{{ $koumten->repair_gaiheki }}&nbsp;~</td>
-            </tr>
-            <tr>
-                <td>修理(内装)&nbsp;&nbsp;</td>
-                <td>{{ $koumten->repair_naisou }}&nbsp;~</td>
-            </tr>
-            <tr>
-                <td>修理(水漏れ)&nbsp;&nbsp;</td>
-                <td>{{ $koumten->repair_mizumore }}&nbsp;~</td>
-            </tr>
-            <tr>
-                <td>修理(エアコン)&nbsp;&nbsp;</td>
-                <td>{{ $koumten->repair_aircon }}&nbsp;~</td>
-            </tr>
-        </table>
-        <div class="block text-gray-700  mt-5 border-l-2 pl-2 mb-2">対応可能エリア</div>
-        <h3>{{ $koumten->avairable_area }}</h3>
-        <div class="block text-gray-700  mt-5 border-l-2 pl-2 mb-2">電話番号</div>
-        <h3>{{ $koumten->contact_address_phone }}</h3>
-        <div class="block text-gray-700 mt-5 border-l-2 pl-2 mb-2">メールアドレス</div>
-        <h3>{{ $koumten->contact_address_mail }}</h3>
-        {{-- <form action="{{ route('button1') }}" method="GET">
+        <div class="container flex">
+            <div>
+                <div class="block text-gray-700  mt-5 border-l-2 pl-2 mb-2 mr-5">住所</div>
+                {{ $koumten->address }}
+                <div class="block text-gray-700  mt-5 mb-1 border-l-2 pl-2 mb-2">対応可能工事一覧 (単位:万円)</div>
+                <table>
+                    <tr>
+                        <td>新築&nbsp;&nbsp;</td>
+                        <td>{{ $koumten->sinchiku }}&nbsp;~</td>
+                    </tr>
+                    <tr>
+                        <td>リフォーム&nbsp;&nbsp;</td>
+                        <td>{{ $koumten->reform }}&nbsp;~</td>
+                    </tr>
+                    <tr>
+                        <td>修理(雨漏り)&nbsp;&nbsp;</td>
+                        <td>{{ $koumten->repair_amamori }}&nbsp;~</td>
+                    </tr>
+                    <tr>
+                        <td>修理(外壁)&nbsp;&nbsp;</td>
+                        <td>{{ $koumten->repair_gaiheki }}&nbsp;~</td>
+                    </tr>
+                    <tr>
+                        <td>修理(内装)&nbsp;&nbsp;</td>
+                        <td>{{ $koumten->repair_naisou }}&nbsp;~</td>
+                    </tr>
+                    <tr>
+                        <td>修理(水漏れ)&nbsp;&nbsp;</td>
+                        <td>{{ $koumten->repair_mizumore }}&nbsp;~</td>
+                    </tr>
+                    <tr>
+                        <td>修理(エアコン)&nbsp;&nbsp;</td>
+                        <td>{{ $koumten->repair_aircon }}&nbsp;~</td>
+                    </tr>
+                </table>
+                <div class="block text-gray-700  mt-5 border-l-2 pl-2 mb-2">対応可能エリア</div>
+                <h3>{{ $koumten->avairable_area }}</h3>
+                <div class="block text-gray-700  mt-5 border-l-2 pl-2 mb-2">電話番号</div>
+                <h3>{{ $koumten->contact_address_phone }}</h3>
+                <div class="block text-gray-700 mt-5 border-l-2 pl-2 mb-2">メールアドレス</div>
+                <h3>{{ $koumten->contact_address_mail }}</h3>
+                {{-- <form action="{{ route('button1') }}" method="GET">
             <input type="submit" value="会社一覧に戻る">
         </form> --}}
-        <div class="flex">
-            <div class="block text-gray-700 text-sm mb-7 mt-5 hover:text-gray-500 m-3 ">
+            </div>
+            <br>
+            <div class="mt-3">
+                <div id="map" style="width:1000px; height:600px"></div>
+            </div>
+        </div>
+        <div class="flex justfy-content">
+            <div class="block text-gray-700 text-sm mb-7 mt-5 hover:text-gray-500 mr-3 ">
                 <form action="{{ route('koumtens.edit', $koumten) }}">
                     <button type="submit" name="top" onclick="location.href=">会社情報を編集する</button>
                 </form>
             </div>
-            <div class="block text-gray-700 text-sm mb-7 mt-5 hover:text-gray-500 m-3 ">
+            <div class="block text-gray-700 text-sm mb-7 mt-5 hover:text-gray-500 mr-3 ">
                 <form action="{{ route('koumtens.destroy', $koumten) }}" method="POST">
-                @csrf
-                @method('delete')
+                    @csrf
+                    @method('delete')
                     <button type="submit" name="top" onclick="location.href=">会社情報を削除する</button>
                 </form>
             </div>
         </div>
 
-        <br>
-        <div id="map" style="width:1000px; height:600px"></div>
         @include('partial.map')
         <script>
             @if (!empty($koumten))
